ec0e792815f018472fbb4571d2a140f76aa9503a
Ravindra Aditya <me@ravindra.pro>
2020-10-16 14:18:51 +0530

    Added few rules and order details

---
+++ PromotionEngineConsoleHub/PromotionEngineConsoleHub/OrderInfo/OrderDetails.cs
@@ -0,0 +1,38 @@
+﻿using PromotionEngineConsoleHub.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PromotionEngineConsoleHub.OrderInfo
+{
+    /// <summary>
+    /// Order Details
+    /// </summary>
+    public class OrderDetails
+    {
+        /// <summary>
+        /// Get Applicable Promotion 1 On Order
+        /// </summary>
+        /// <param name="items">basket items</param>
+        /// <returns>Promotion Details</returns>
+        public ValueTuple<Promotion, bool> GetApplicablePromotion1OnOrder(List<BasketItems> items)
+        {
+            var promotionDetails = new ValueTuple<Promotion, bool>();
+            promotionDetails.Item1 = new Promotion() { PromotionID = "AP1", PrintText = "Promotion 1" };
+            promotionDetails.Item2 = true;
+            return promotionDetails;
+        }
+
+        /// <summary>
+        /// Get Applicable Promotion 1 On Order
+        /// </summary>
+        /// <param name="items">basket items</param>
+        /// <returns>Promotion Details</returns>
+        public ValueTuple<Promotion, bool> GetApplicablePromotion2OnOrder(List<BasketItems> items)
+        {
+            var promotionDetails = new ValueTuple<Promotion, bool>();
+            promotionDetails.Item1 = new Promotion() { PromotionID = "AP2", PrintText = "Promotion 2" };
+            promotionDetails.Item2 = true;
+            return promotionDetails;
+        }
+    }
+}
--- PromotionEngineConsoleHub/PromotionEngineConsoleHub/RulesEngine/RulesEngine.cs
+++ PromotionEngineConsoleHub/PromotionEngineConsoleHub/RulesEngine/RulesEngine.cs
@@ -1,8 +1,6 @@
 ﻿using PromotionEngineConsoleHub.Models;
-using PromotionEngineConsoleHub.Order;
-using System;
+using PromotionEngineConsoleHub.OrderInfo;
 using System.Collections.Generic;
-using System.Text;
 
 namespace PromotionEngineConsoleHub.RulesEngine
 {
@@ -12,16 +10,26 @@ namespace PromotionEngineConsoleHub.RulesEngine
     public class RulesEngine
     {
         /// <summary>
-        /// basket items
+        /// Basket items
         /// </summary>
         public List<BasketItems> Items { get; private set;  }
+
+        /// <summary>
+        /// Available Promotions
+        /// </summary>
+        public List<Promotion> AvailablePromotions { get; private set; }
+     
+        /// <summary>
+        /// Order Information
+        /// </summary>
+        public Order Order { get; set; }
 
         /// <summary>
         /// Rules Engine Constructor
         /// </summary>
         public RulesEngine()
         {
-
+            this.Order = new Order();
         }
 
         /// <summary>
@@ -32,7 +40,19 @@ namespace PromotionEngineConsoleHub.RulesEngine
         public bool PopulateBasketItems(List<BasketItems> items)
         {
             //Replace with api call.
+            this.Order.Items = items;
             this.Items = items;
+            return true;
+        }
+
+        /// <summary>
+        /// Populate available promotions
+        /// </summary>
+        /// <param name="promotions">available promotions</param>
+        /// <returns>boolean based on api success or failure</returns>
+        public bool PopulateAvailablePromotions(List<Promotion> promotions)
+        {
+            this.AvailablePromotions = promotions;
             return true;
         }
 
@@ -44,8 +64,21 @@ namespace PromotionEngineConsoleHub.RulesEngine
         {
             /* Rule 1 */
             var promotion1 = new OrderDetails().GetApplicablePromotion1OnOrder(this.Items);
+
+            if(promotion1.Item2)
+            {
+                this.Order.Promotions.Add(promotion1.Item1);
+            }
+
+
             /* Rule 2 */
             var promotion2 = new OrderDetails().GetApplicablePromotion2OnOrder(this.Items);
+
+            if (promotion2.Item2)
+            {
+                this.Order.Promotions.Add(promotion2.Item1);
+            }
+
             return true;
         }
     }
